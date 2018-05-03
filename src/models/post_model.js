ce097b482451cd2d86f4f0cb9842d4d318254ee3
andersbando-hess <bandersman@gmail.com>
2018-05-03 19:00:35 -0400

    quick fix

---
+++ src/models/post_model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PostSchema = new Schema({
+  title: String,
+  tags: String,
+  content: String,
+  cover_url: String,
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+});
+
+// create model class
+const PostModel = mongoose.model('Post', PostSchema);
+
+export default PostModel;
